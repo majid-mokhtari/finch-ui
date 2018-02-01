33c79bca1b471d90c918de79c0b6b1cf1472b616
Majid Mokhtari <majidmokht@gmail.com>
2018-01-31 17:53:45 -0800

    Picture description

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
-import Me from './src/images/me.jpeg';
+import Me from './src/images/we-asked-women-what-they-find-attractive-on-tinder-876-body-image-1469617144-size_1000.jpeg';
 
 const fbImage = 'https://graph.facebook.com/259389830744794/picture?height=500';
+const reactImage = 'http://geekycentral.com/wp-content/uploads/2017/09/react-native.png';
 
 export default class App extends React.Component {
   render() {
@@ -10,8 +11,12 @@ export default class App extends React.Component {
       <View style={styles.card}>
         <Image 
           style={{flex: 1}}
-          source={Me}
+          source={{ uri: reactImage}}
         />
+        <View style={{margin: 20}} >
+          <Text style={{fontSize: 28}} >Christine, 26</Text>
+          <Text style={{fontSize: 15, color: 'darkgrey'}} >Alabama</Text> 
+        </View>
       </View>
     );
   }
@@ -20,6 +25,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   card: {
     flex: 1,
+    overflow: 'hidden', 
     backgroundColor: 'white',
     margin: 10,
     marginTop: 100,
