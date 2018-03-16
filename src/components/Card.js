482add66822031bf0817bbd7bfd635a66053d047
Majid Mokhtari <majidmokht@gmail.com>
2018-03-16 14:37:46 -0700

    card swiper

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,103 @@
-const fbImage = 'https://graph.facebook.com/259389830744794/picture?height=500';
-
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+import { Image, StyleSheet } from 'react-native';
+import { Container, Header, Button, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+
 const cards = [
   {
-    text: 'Card One',
     name: 'Angela, 28',
     city: 'Alabama',
-    image: fbImage,
+    image: 'https://graph.facebook.com/259389830744794/picture?height=500',
   },
 ];
 
 class CardComponent extends Component {
+
+  onIconRightPress(){
+    this._deckSwiper._root.swipeRight();
+  }
+
+  onIconLeftPress(){
+    this._deckSwiper._root.swipeLeft();
+  }
+
+  onSwipeRight(){ 
+    console.log("Right")
+  }
+
+  onSwipeLeft(){
+    console.log("Left")
+  }
+
   render() {
     return (
       <Container>
         <Header />
         <View>
           <DeckSwiper
+            ref={(c) => this._deckSwiper = c}
             dataSource={cards}
-            onSwipeRight={() => console.log("Right")}
-            onSwipeLeft={() => console.log("Left")}
+            onSwipeRight={this.onSwipeRight.bind(this)}
+            onSwipeLeft={this.onSwipeLeft.bind(this)}
             renderItem={item =>
-              <Card style={{ elevation: 3 }}>
+              <Card style={styles.cardStyle}>
                 <CardItem cardBody>
                   <Image 
-                    style={{ height: 400, flex: 1 }} 
+                    style={styles.imageStyle} 
                     source={{uri: item.image}} 
                   />
                 </CardItem>
                 <CardItem>
-                  <Text>{item.name}</Text>
+                  <Text style={styles.nameText}>{item.name}</Text>
+                </CardItem>
+                <CardItem>
                   <Text>{item.city}</Text>
                 </CardItem>
               </Card>
             }
           />
         </View>
+        <View style={styles.footerStyle}>
+          <Button 
+            iconLeft onPress={this.onIconLeftPress.bind(this)}
+          >
+            <Icon name="arrow-back" />
+            <Text>Swipe Left</Text>
+          </Button>
+          <Button 
+            iconRight onPress={this.onIconRightPress.bind(this)}
+          >
+            <Text>Swipe Right</Text>
+            <Icon name="arrow-forward" />
+          </Button>
+        </View>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cardStyle: {
+    elevation: 3,
+  },
+  nameText: {
+    fontSize: 28,
+  },
+  imageStyle: {
+    height: 450, 
+    flex: 1,
+    borderRadius: 10
+  },
+  footerStyle: {
+    flexDirection: "row", 
+    flex: 1, 
+    position: "absolute", 
+    bottom: 50, 
+    left: 0, 
+    right: 0, 
+    justifyContent: 'space-between', 
+    padding: 15 
+  }
+});
 
 export default CardComponent;
 
@@ -49,6 +105,7 @@ export default CardComponent;
 // import { StyleSheet, Text, View, Image} from 'react-native';
 
 // const reactImage = 'http://geekycentral.com/wp-content/uploads/2017/09/react-native.png';
+//const fbImage = 'https://graph.facebook.com/259389830744794/picture?height=500';
 
 // class Cards extends Component {
 //   render() {
